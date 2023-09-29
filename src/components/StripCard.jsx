6275a11dd96db7ed08264f2be2cbf80b11ae74e0
Mumin Bhat <mumin.fayaz5050@gmail.com>
2023-09-29 10:46:10 +0530

    web app structured

---
+++ src/components/StripCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StripCard = () => {
+  return (
+    <div>StripCard</div>
+  )
+}
+
+export default StripCard
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import StripCard from '../components/StripCard'
+
+const HomePage = () => {
+  return (
+    <>
+    <Hero />
+    <StripCard />
+    </>
+  )
+}
+
+export default HomePage
