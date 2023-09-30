859259695251e96ab4bdb3254e2f37c37094bd97
Mumin Bhat <mumin.fayaz5050@gmail.com>
2023-09-30 17:07:24 +0530

    ui enhancements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -222,7 +222,7 @@ const Hero = () => {
                     className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md bg-teal-300 border border-transparent font-semibold text-black-500 hover:text-white hover:bg-teal-600 focus:outline-none focus:ring-2 ring-offset-white focus:ring-purple-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
                     href="/"
                   >
-                    Upload{" "}
+                    Get Started{" "}
                   </button>
                 </div>
               </div>
